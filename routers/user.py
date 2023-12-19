9e7977d052ec9fd5c09d56e7a79ebd3f31ff8115
raghuramjee7 <raghuramjee@outlook.com>
2023-12-19 23:41:16 +0530

    Password rules updated

--- routers/user.py
+++ routers/user.py
@@ -25,16 +25,16 @@ def password_conditions(password: str):
     if len(password) < 8:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password must be at least 8 characters long")
     
-    if '0123456789' not in password:
+    if not any([char not in '0123456789' for char in password]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password must contain at least one number")
     
-    if 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' not in password:
+    if not any([char not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for char in password]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password must contain at least one uppercase letter")
     
-    if 'abcdefghijklmnopqrstuvwxyz' not in password:
+    if not any([char not in 'abcdefghijklmnopqrstuvwxyz' for char in password]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password must contain at least one lowercase letter")
     
-    if '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' not in password:
+    if not any([char not in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' for char in password]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password must contain at least one special character")
     
 
